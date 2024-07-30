a3d1c6ccdf5ada4958e377639ec29a4c34e0b507
mateodr17 <mateodarosa132@gmail.com>
2024-07-30 10:42:15 -0300

    intento de lote

--- header/lotes.php
+++ header/lotes.php
@@ -1,3 +1,27 @@
+<?php
+
+$conexion = mysqli_connect("localhost", "root", "", "proyecto") or die("Datos incorrectos");
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['titulo']) && isset($_FILES['archivo'])) {
+        if (!empty($_POST["titulo"]) && !empty($_FILES["archivo"])){
+
+            $titulo = $_POST['titulo'];
+            $foto_lote = $_FILES['archivo']['name'];
+
+            if ($nombre_archivo != '') {
+                $nombre_archivo_unico = $titulo . '_' . $foto_lote;
+                $ruta_archivo = 'foto_lote/' . $titulo .'/' . $nombre_archivo_unico;
+        
+                if (!file_exists('foto_lote/' . $titulo'')) {
+                    if (!mkdir('foto_lote/' . $titulo'', 0777, true)) {
+                        die("Error al crear el directorio.");
+                    }
+                }
+            }
+        }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +30,16 @@
     <title>Document</title>
 </head>
 <body>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam modi aliquam dicta exercitationem temporibus placeat consectetur. Dolor laborum adipisci, illo animi rerum quas. Incidunt asperiores dolorum commodi placeat velit necessitatibus.</p>
+    <form action="" method="post">
+        <label for="titulo_lote">
+            Titulo del lote:
+            <input type="text" name="titulo" id="">
+        </label>
+        <label for="foto_lote">
+            Foto principal del lote:
+        <input type="file" name="archivo" required accept='.jpg , .png'>
+        </label>
+        <input type="button" value="">
+    </form>
 </body>
 </html>
